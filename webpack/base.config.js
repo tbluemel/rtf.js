ebdedc1998d17f3d95134f7fa29ee335c5e56eff
Tom Z?hner <tom.zoehner@gmail.com>
2018-03-24 16:33:59 +0100

    Switch coverage reporting to webpack

---
+++ webpack/base.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    node: false,
+    entry: {
+        EMFJS: './src/emfjs/index.ts',
+        RTFJS: './src/rtfjs/index.ts',
+        WMFJS: './src/wmfjs/index.ts'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+        alias: {
+            EMFJS:  path.resolve(__dirname, '../src/emfjs/index.ts'),
+            WMFJS:  path.resolve(__dirname, '../src/wmfjs/index.ts'),
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].bundle.js',
+        library: '[name]',
+        libraryTarget: 'umd'
+    },
+    externals: {
+        EMFJS: 'EMFJS',
+        WMFJS: 'WMFJS'
+    }
+};
---
+++ webpack/coverage.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./base.config.js');
+
+module.exports = merge(baseConfig, {
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: [
+                    {
+                        loader: 'istanbul-instrumenter-loader',
+                        options: {
+                            esModules: true
+                        }
+                    },
+                    'ts-loader'
+                ],
+                include: [
+                    path.resolve(__dirname, "../src"),
+                    path.resolve(__dirname, "../node_modules/codepage")
+                ]
+            }
+        ]
+    }
+});
